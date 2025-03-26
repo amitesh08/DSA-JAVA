0fad97b8cdb74a4b207935b999c6b0f9a69550c0
amitesh08 <aamitesh125@gmail.com>
2025-03-26 11:59:30 +0530

    recursion

--- Codes/10-Recursion/src/com/amitesh/Power.java
+++ Codes/10-Recursion/src/com/amitesh/Power.java
@@ -2,7 +2,8 @@ package com.amitesh;
 
 public class Power {
     public static void main(String[] args) {
-        System.out.println(power(2,4));
+        System.out.println(power(2,6));
+        System.out.println(optimalSolution(2,5));
     }
     public static int power(int a, int n){
         if(n == 1){
@@ -12,5 +13,21 @@ public class Power {
         int temp = power(a,n-1);
 
         return a*temp;
+    }
+    public static int optimalSolution(int a , int n ){
+        if(n == 1){
+            return a;
+        }
+
+        int temp = power(a,n/2);
+
+        if (oddEven(n)) return temp*temp;
+
+        return temp*temp*a;
+    }
+    public static boolean oddEven(int n ){
+        if (n % 2 != 0) return false;
+        return true;
+
     }
 }
