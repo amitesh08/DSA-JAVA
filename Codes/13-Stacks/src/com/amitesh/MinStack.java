3294eb17a32d75304262899632ff7b55d43cd508
amitesh08 <aamitesh125@gmail.com>
2025-07-24 08:11:33 +0530

    Min stack

---
+++ Codes/13-Stacks/src/com/amitesh/MinStack.java
@@ -0,0 +1,47 @@
+package com.amitesh;
+
+import java.util.Stack;
+
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int removed = stack.pop();
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack ms = new MinStack();
+        ms.push(5);
+        ms.push(3);
+        ms.push(7);
+        System.out.println(ms.getMin()); // 3
+        ms.pop();
+        System.out.println(ms.getMin()); // 3
+        ms.pop();
+        System.out.println(ms.getMin()); // 5
+    }
+}
